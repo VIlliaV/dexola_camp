325abe3ba2e7278084fc380b4397b425d318e7be
Illia <illia.vasiliev@gmail.com>
2023-08-23 00:23:00 +0300

    done footer and header desk

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
+  border-top: 1px solid var(--accent);
   & .container {
+    position: relative;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     justify-items: center;
     align-items: center;
     padding: 34px 0;
 
+    ::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 360px;
+      bottom: 0;
+      width: 1px;
+      background-color: var(--accent);
+    }
+    ::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 360px;
+      bottom: 0;
+      width: 1px;
+      background-color: var(--accent);
+    }
     & .footer_item_1 {
       justify-self: start;
     }
