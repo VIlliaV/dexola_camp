137f2f91c261c876ac143d5db4243c171154ac52
Illia <illia.vasiliev@gmail.com>
2023-08-22 08:58:05 +0300

    done header style

--- src/AppStyle.styled.js
+++ src/AppStyle.styled.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   min-height: 100vh;
   display: flex;
+
   flex-direction: column;
   & main {
     flex: 1;
+    margin-top: 78px;
   }
 `;
