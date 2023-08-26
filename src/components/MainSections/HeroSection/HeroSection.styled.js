0a2a7d30ee2c0933737cf636ec12139a437bb728
Illia <illia.vasiliev@gmail.com>
2023-08-26 21:20:03 +0300

    UPD hero section

--- src/components/MainSections/HeroSection/HeroSection.styled.js
+++ src/components/MainSections/HeroSection/HeroSection.styled.js
@@ -1,51 +1,92 @@
 import styled from 'styled-components';
+import { media } from '@/styles/media';
+
 const HEIGHT_HERO_PRESENTATION = '311px';
 
 export const PresentationContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
   grid-template-rows: ${HEIGHT_HERO_PRESENTATION} auto;
 
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 1px;
-    width: 100vw;
-    background-color: var(--accent);
+  @media ${media.tabDesk} {
+    grid-template-columns: repeat(1, 1fr);
+    //  варіант №2
+    /* grid-template-rows: calc(${HEIGHT_HERO_PRESENTATION}*2) auto; */
   }
-  &::before {
-    content: '';
-    position: absolute;
-    top: ${HEIGHT_HERO_PRESENTATION};
-    left: 0;
-    height: 1px;
-    width: 100vw;
-    background-color: var(--accent);
+  @media ${media.desktop} {
+    grid-template-columns: repeat(2, 1fr);
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 1px;
+      width: 100vw;
+      background-color: var(--accent);
+    }
+    &::before {
+      content: '';
+      position: absolute;
+      top: ${HEIGHT_HERO_PRESENTATION};
+      left: 0;
+      height: 1px;
+      width: 100vw;
+      background-color: var(--accent);
+    }
   }
+
   & .hero_item {
-    /* display: flex;
-    align-items: flex-start; */
+    &:nth-child(1) {
+      order: 2;
+    }
+    @media ${media.desktop} {
+      &:nth-child(2) {
+        order: 2;
+      }
+    }
   }
   & .hero_item_slider {
     display: flex;
+    justify-content: space-evenly;
+
+    @media ${media.desktop} {
+      justify-self: end;
+    }
+    & .image-slider {
+      width: 50%;
+      @media ${media.tabDesk} {
+        //  варіант №1
+        max-width: 370px;
+        //  варіант №2
+        /* & .cut-container {
+          width: 740px;
+          height: 740px;
+        } */
+      }
+      @media ${media.desktop} {
+        width: 233px;
+      }
+    }
   }
   & .hero_item_presentation-text {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-self: stretch;
-    padding: 0 70px 0px 24px;
+    @media ${media.desktop} {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      align-self: stretch;
+      padding-left: 24px;
 
-    & p {
-      flex: 0 0 50%;
-      display: block;
-      width: 640px;
+      & p {
+        flex: 0 0 50%;
+        display: block;
+        width: 640px;
+      }
     }
   }
   & .hero_item_run {
-    grid-column: span 2;
-    grid-row: 2;
+    @media ${media.desktop} {
+      grid-column: span 2;
+      grid-row: 2;
+    }
   }
 `;
