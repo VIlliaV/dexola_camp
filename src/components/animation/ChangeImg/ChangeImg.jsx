2841406b09a0c343dd66a69d81f863a76f58015f
Illia <illia.vasiliev@gmail.com>
2023-08-30 00:29:30 +0300

    FIX images

--- src/components/animation/ChangeImg/ChangeImg.jsx
+++ src/components/animation/ChangeImg/ChangeImg.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { media } from '@/styles/media';
 
 import { ImageSlider } from './ChangeImg.styled';
 
@@ -20,8 +21,15 @@ const ChangeImg = ({ nftData = [], initial = 0, step = 1, period = 3000 }) => {
       {nftData.map((item, index) => (
         <div className={`cut-container ${index === activeImage ? 'active' : ''}`} key={index}>
           <picture>
-            <source srcSet={item.src2} media="(-webkit-min-device-pixel-ratio: 2)" />
-            <img src={item.src} alt={item.name} className={`slider-image ${index === activeImage ? 'active' : ''}`} />
+            <source srcSet={`${item.src.desk} 1x, ${item.src.desk2x} 2x`} media={media.desktop} />
+            <source srcSet={`${item.src.tab} 1x, ${item.src.desk} 2x`} media={media.tablet} />
+            <source srcSet={`${item.src.mob} 1x, ${item.src.tab} 2x`} media={media.mobile} />
+
+            <img
+              src={item.src.jpg}
+              alt={item.name}
+              className={`slider-image ${index === activeImage ? 'active' : ''}`}
+            />
           </picture>
         </div>
       ))}
