e815eae11e4123209efdeac20fb49c4b52ee8151
Illia <illia.vasiliev@gmail.com>
2023-08-31 23:24:14 +0300

    FIX backend filename for img

--- src/components/MainSections/Join/Join.jsx
+++ src/components/MainSections/Join/Join.jsx
@@ -1,10 +1,32 @@
 import Section from '../../Section/Section';
 import Title from '../../Title/Title';
+import { JoinConteiner } from './Join.styled';
 
 const Join = ({ numberSection }) => {
   return (
     <Section>
       <Title numberSection={numberSection}> join us</Title>
+      <JoinConteiner>
+        <div>
+          <h3>Experience the Power of StarRunner</h3>
+          <p>
+            Join Our Community and Embark on a Journey of Opportunities. Discover the full range of our services that
+            cater to your needs. We`ve got you covered with top-notch solutions.
+          </p>
+          <p>---------------???</p>
+        </div>
+        <form>
+          <label>Email:</label>
+          <input type="email" />
+          <label>тел:</label>
+          <input type="tel" />
+          <label>password:</label>
+          <input type="password" />
+          <label>confirm password:</label>
+          <input type="password" />
+          <button type="submit">SEND IT</button>
+        </form>
+      </JoinConteiner>
     </Section>
   );
 };
---
+++ src/components/MainSections/Join/Join.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const JoinConteiner = styled.div`
+  display: flex;
+`;
